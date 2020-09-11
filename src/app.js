363652f06546908f077bd2d6d8ac83c689f94918
Valentyn Filippov <valentine@Valentyns-MacBook-Pro.local>
2020-09-12 00:34:09 +0300

    created blank

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
 import './styles/styles.scss';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(<p>app paragraph</p>, document.getElementById('app'));

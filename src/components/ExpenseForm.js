5b778c955af81131a77d636a95ad295ee5592edb
Valentyn Filippov <valentine@Valentyns-MacBook-Pro.local>
2020-09-17 19:13:13 +0300

    npx react-codemod rename-unsafe-lifecycles

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,18 +1,93 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+import { addExpense } from '../redux/actions/expenses';
+
+const now = moment();
+console.log(now.format('MMM Do, YYYY'));
 
 class ExpenseForm extends Component {
-  state = {};
+  state = {
+    description: '',
+    amount: '',
+    note: '',
+    createdAt: moment(),
+    calendarFocused: false,
+  };
+
+  handleChange = ({ target: { value, name } }) => {
+    this.setState({ [name]: value });
+  };
+
+  handleAmountChange = ({ target: { value, name } }) => {
+    if (value.match(/^\d*(\.\d{0,2})?$/)) {
+      this.setState({ [name]: Number(value) });
+    }
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { description, amount, note, createdAt } = this.state;
+    this.props.dispatch(addExpense({ description, amount, note, createdAt }));
+    this.cleanFields();
+  };
+
+  cleanFields() {
+    this.setState({ description: '', amount: 0, note: '' });
+  }
+
+  handleDateChange = (createdAt) => {
+    this.setState({ createdAt });
+  };
+
+  handleCalendarFocusChange = ({ focused }) => {
+    this.setState({ calendarFocused: focused });
+  };
 
   render() {
+    const {
+      description,
+      amount,
+      note,
+      createdAt,
+      calendarFocused,
+    } = this.state;
     return (
-      <form action=''>
-        <input type='text' />
-        <input type='text' />
-        <input type='text' />
-        <input type='text' />
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type='text'
+          placeholder='Description'
+          autoFocus
+          name='description'
+          value={description}
+          onChange={this.handleChange}
+        />
+        <input
+          type='text'
+          placeholder='Amount'
+          name='amount'
+          value={amount}
+          onChange={this.handleAmountChange}
+        />
+        <SingleDatePicker
+          date={createdAt}
+          onDateChange={this.handleDateChange}
+          focused={calendarFocused}
+          onFocusChange={this.handleCalendarFocusChange}
+        />
+        <textarea
+          placeholder='Add a note for your expense (optional)'
+          name='note'
+          value={note}
+          onChange={this.handleChange}
+        ></textarea>
+        <button type='submit'>Add Expense</button>
       </form>
     );
   }
 }
 
-export default ExpenseForm;
+export default connect()(ExpenseForm);
